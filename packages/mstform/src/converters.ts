9297cef29864234b9c17d8460391106bab2e4520
Martijn Faassen <faassen@startifact.com>
2018-05-15 17:58:36 +0200

    A decimal validator.

--- packages/mstform/src/converters.ts
+++ packages/mstform/src/converters.ts
@@ -6,7 +6,7 @@ import {
   IConverter
 } from "./converter";
 
-const NUMBER_REGEX = new RegExp("^-?(0|[1-9]\\d*)(\\.\\d+)?$");
+const NUMBER_REGEX = new RegExp("^-?(0|[1-9]\\d*)(\\.\\d*)?$");
 const INTEGER_REGEX = new RegExp("^-?(0|[1-9]\\d*)$");
 
 const string = new Converter<string, string>({
@@ -23,6 +23,10 @@ const string = new Converter<string, string>({
 
 const number = new Converter<string, number>({
   rawValidate(raw) {
+    // deal with case when string starts with .
+    if (raw.startsWith(".")) {
+      raw = "0" + raw;
+    }
     return NUMBER_REGEX.test(raw);
   },
   convert(raw) {
@@ -50,6 +54,51 @@ const integer = new Converter<string, number>({
     return value;
   }
 });
+
+class Decimal implements IConverter<string, string> {
+  public converter: Converter<string, string>;
+
+  constructor(public maxWholeDigits: number, public decimalPlaces: number) {
+    const regex = new RegExp(
+      `^-?(0|[1-9]\\d{0,${maxWholeDigits - 1}})(\\.\\d{0,${decimalPlaces}})?$`
+    );
+    this.converter = new Converter<string, string>({
+      rawValidate(raw) {
+        // deal with case when string starts with .
+        if (raw.startsWith(".")) {
+          raw = "0" + raw;
+        }
+        return regex.test(raw);
+      },
+      convert(raw) {
+        return raw;
+      },
+      render(value) {
+        return value;
+      },
+      getRaw(value: any) {
+        return value;
+      }
+    });
+  }
+
+  convert(raw: string) {
+    return this.converter.convert(raw);
+  }
+  render(value: string) {
+    return this.converter.render(value);
+  }
+  getRaw(value: any) {
+    return value;
+  }
+}
+
+function decimal(
+  maxDigits: number,
+  decimalPlaces: number
+): IConverter<string, string> {
+  return new Decimal(maxDigits, decimalPlaces);
+}
 
 class Maybe<V> implements IConverter<string, V | null> {
   constructor(public converter: Converter<string, V>) {}
@@ -95,6 +144,7 @@ export const converters = {
   string,
   number,
   integer,
+  decimal,
   stringArray,
   maybe
 };
--- packages/mstform/test/converters.ts
+++ packages/mstform/test/converters.ts
@@ -28,8 +28,41 @@ test("string converter", async () => {
 test("number converter", async () => {
   await check(converters.number, "3", 3);
   await check(converters.number, "3.14", 3.14);
+  await check(converters.number, ".14", 0.14);
+  await check(converters.number, "19.14", 19.14);
+  await check(converters.number, "19.", 19);
   await check(converters.number, "-3.14", -3.14);
   await fails(converters.number, "foo");
+  await fails(converters.number, "1foo");
+  await fails(converters.number, "");
+});
+
+test("integer converter", async () => {
+  await check(converters.integer, "3", 3);
+  await fails(converters.integer, "3.14");
+  await fails(converters.integer, ".14");
+  await check(converters.integer, "0", 0);
+  await check(converters.integer, "-3", -3);
+  await fails(converters.integer, "foo");
+  await fails(converters.integer, "1foo");
+  await fails(converters.integer, "");
+});
+
+test("decimal converter", async () => {
+  await check(converters.decimal(4, 2), "3", "3");
+  await check(converters.decimal(4, 2), "3.14", "3.14");
+  await check(converters.decimal(4, 2), "43.14", "43.14");
+  await check(converters.decimal(4, 2), "4313", "4313");
+
+  await check(converters.decimal(4, 2), "-3.14", "-3.14");
+  await check(converters.decimal(4, 2), "0", "0");
+  await check(converters.decimal(4, 2), ".14", ".14");
+  await check(converters.decimal(4, 2), "14.", "14.");
+  await fails(converters.decimal(4, 2), "foo");
+  await fails(converters.decimal(4, 2), "1foo");
+  await fails(converters.decimal(4, 2), "");
+  await fails(converters.decimal(4, 2), "12345.34");
+  await fails(converters.decimal(4, 2), "12.444");
 });
 
 test("maybe number converter", async () => {
