69120f26ab51456f1a9fb41257151583acfb0d3c
Martijn Faassen <faassen@startifact.com>
2018-08-16 12:18:03 +0200

    Confirm is works for repeating forms too.

--- test/group.test.ts
+++ test/group.test.ts
@@ -1,6 +1,6 @@
 import { configure } from "mobx";
 import { types } from "mobx-state-tree";
-import { Field, Form, Group, converters } from "../src";
+import { Field, Form, Group, RepeatingForm, converters } from "../src";
 
 configure({ enforceActions: true });
 
@@ -53,4 +53,53 @@ test("a group form", async () => {
   await field2.setRaw("illegal");
   expect(await groupBState.validate()).toBeFalsy();
   expect(await groupAState.validate()).toBeTruthy();
+});
+
+test("group repeating form", async () => {
+  const N = types.model("N", {
+    a: types.string,
+    b: types.number,
+    c: types.string,
+    d: types.number
+  });
+
+  const M = types.model("M", {
+    foo: types.array(N)
+  });
+
+  const form = new Form(M, {
+    foo: new RepeatingForm({
+      a: new Field(converters.string),
+      b: new Field(converters.number),
+      c: new Field(converters.string),
+      d: new Field(converters.number)
+    })
+  });
+
+  const groupAB = new Group(N, ["a", "b"]);
+  const groupCD = new Group(N, ["c", "d"]);
+
+  const o = M.create({ foo: [{ a: "A", b: 1, c: "C", d: 2 }] });
+
+  const state = form.state(o);
+
+  const forms = state.repeatingForm("foo");
+  const oneForm = forms.index(0);
+
+  const oneFormAB = groupAB.access(oneForm);
+  const field = oneFormAB.field("b");
+
+  expect(field.raw).toEqual("1");
+  await field.setRaw("10");
+  expect(field.raw).toEqual("10");
+  expect(field.value).toEqual(10);
+
+  const oneFormCD = groupCD.access(oneForm);
+
+  expect(await oneFormAB.validate()).toBeTruthy();
+  expect(await oneFormCD.validate()).toBeTruthy();
+
+  await field.setRaw("illegal");
+  expect(await oneFormAB.validate()).toBeFalsy();
+  expect(await oneFormCD.validate()).toBeTruthy();
 });
