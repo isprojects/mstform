9994a2e37155317355cd9e6f09957a205a16404f
Martijn Faassen <faassen@startifact.com>
2018-10-10 14:41:32 +0200

    Progress in resolveMessage.

--- src/messages.ts
+++ src/messages.ts
@@ -1,10 +1,12 @@
-import { pathToSteps } from "./utils";
+import { pathToSteps, isInt } from "./utils";
 
 type MessagesArray = MessagesObject[];
 type MessagesObject = {
   [key: string]: string | MessagesObject | MessagesArray;
 };
-type Messages = undefined | MessagesObject;
+export type Messages = undefined | MessagesObject;
+
+type SomeMessage = MessagesObject | MessagesArray | string | undefined;
 
 export function resolveMessage(
   messages: Messages,
@@ -17,20 +19,26 @@ function resolveSteps(messages: Messages, steps: string[]): string | undefined {
   if (messages === undefined) {
     return undefined;
   }
-  const [first, ...rest] = steps;
-  if (messages instanceof Object) {
+  if (steps.length === 0) {
+    return getError(messages);
   }
+  return resolveStep(messages, steps);
 }
 
-function resolveStep(
-  messages: MessagesObject | MessagesArray | string,
-  steps: string[]
-) {
-  const [first, ...rest] = steps;
-
+function resolveStep(messages: SomeMessage, steps: string[]) {
+  if (messages === undefined) {
+    return undefined;
+  }
   if (typeof messages === "string") {
     return resolveString(messages, steps);
   }
+  if (messages instanceof Array) {
+    return resolveArray(messages, steps);
+  }
+  if (messages instanceof Object) {
+    return resolveObject(messages, steps);
+  }
+  return undefined;
 }
 
 function resolveObject(
@@ -38,12 +46,75 @@ function resolveObject(
   steps: string[]
 ): string | undefined {
   const [first, ...rest] = steps;
-  const value = messages[first];
+  if (rest.length === 0) {
+    return resolveObjectFinalStep(messages, first);
+  }
+  return resolveStep(messages[first], rest);
+}
+
+function resolveObjectFinalStep(
+  messages: MessagesObject,
+  step: string
+): string | undefined {
+  const propError = getPropError(messages, step);
+  if (propError !== undefined) {
+    return propError;
+  }
+  const value = messages[step];
   if (typeof value === "string") {
-    if (rest.length !== 0) {
-      return undefined;
-    }
     return value;
   }
+  if (value instanceof Array) {
+    return undefined;
+  }
+  if (value instanceof Object) {
+    return getError(value);
+  }
+  return undefined;
+}
+
+function getError(value: SomeMessage): string | undefined {
+  if (value === undefined) {
+    return undefined;
+  }
+  if (typeof value === "string") {
+    return undefined;
+  }
+  if (value instanceof Array) {
+    return undefined;
+  }
+  return errorValue(value.__error__);
+}
+
+function getPropError(value: MessagesObject, step: string): string | undefined {
+  return errorValue(value["__error__" + step]);
+}
+
+function errorValue(error: any): string | undefined {
+  if (error === undefined) {
+    return undefined;
+  }
+  if (typeof error !== "string") {
+    return undefined;
+  }
+  return error;
+}
+
+function resolveString(messages: string, steps: string[]): string | undefined {
+  if (steps.length > 0) {
+    return undefined;
+  }
+  return messages;
+}
+
+function resolveArray(
+  messages: MessagesArray,
+  steps: string[]
+): string | undefined {
+  const [first, ...rest] = steps;
+  if (!isInt(first)) {
+    return undefined;
+  }
+  const value = messages[parseInt(first, 10)];
   return resolveStep(value, rest);
 }
--- test/messages.test.ts
+++ test/messages.test.ts
@@ -1,35 +1,81 @@
-import { resolveMessage } from "../src/messages.ts";
+import { resolveMessage, Messages } from "../src/messages";
 
-// test("top-level field", async () => {
-//   // FieldAccessor "foo"
-//   // path: foo
-//   const messages = {
-//     foo: "Wrong"
-//   };
-//   expect(resolveMessage(messages, "foo")).toEqual("Wrong");
-//   expect(resolveMessage(messages, "bar")).toBeUndefined();
-//   expect(resolveMessage(messages, "foo/bar")).toBeUndefined();
-//   expect(resolveMessage(messages, "bar/baz")).toBeUndefined();
-//   expect(resolveMessage(undefined, "foo")).toBeUndefined();
-// });
+test("top-level field", () => {
+  // FieldAccessor "foo"
+  // path: foo
+  const messages = {
+    foo: "Wrong"
+  };
+  expect(resolveMessage(messages, "foo")).toEqual("Wrong");
+  expect(resolveMessage(messages, "bar")).toBeUndefined();
+  expect(resolveMessage(messages, "foo/bar")).toBeUndefined();
+  expect(resolveMessage(messages, "bar/baz")).toBeUndefined();
+  expect(resolveMessage(undefined, "foo")).toBeUndefined();
+});
 
-// FieldAccessor "foo"
-// path: foo
-const fieldError = {
-  foo: "Wrong"
-};
+test("FieldAccessor bar on SubFormAccessor foo", () => {
+  // FieldAccessor "bar" on SubFormAccessor "foo"
+  // path: foo/bar
+  const messages = {
+    foo: {
+      bar: "Wrong"
+    }
+  };
+  expect(resolveMessage(messages, "foo/bar")).toEqual("Wrong");
+  expect(resolveMessage(messages, "foo")).toBeUndefined();
+  expect(resolveMessage(messages, "foo/bar/baz")).toBeUndefined();
+});
 
-// FormState
-// path:
-const formError = {
-  __error__: "Wrong"
-};
+test("FieldAccessor bar on repeating form", () => {
+  // FieldAccessor "bar"
+  // on RepeatingFormIndexed accessor 2 of RepeatingFormAccessor "foo"
+  // path: foo/1/bar
+  const messages: Messages = {
+    foo: [
+      {},
+      {
+        bar: "Wrong"
+      }
+    ]
+  };
+  expect(resolveMessage(messages, "foo/1/bar")).toEqual("Wrong");
+  expect(resolveMessage(messages, "foo/1")).toBeUndefined();
+  expect(resolveMessage(messages, "foo")).toBeUndefined();
+  expect(resolveMessage(messages, "foo/0/bar")).toBeUndefined();
+  expect(resolveMessage(messages, "foo/3/bar")).toBeUndefined();
+});
 
-// RepeatingFormAccessor "foo"
-// path: foo
-const arrayError = {
-  __error__foo: "Wrong"
-};
+test("FormState error", () => {
+  // FormState
+  // path:
+  const messages = {
+    __error__: "Wrong"
+  };
+
+  expect(resolveMessage(messages, "")).toEqual("Wrong");
+});
+
+test("SubForm error", () => {
+  // SubFormAccessor "foo"
+  // path: foo
+  const messages = {
+    foo: {
+      __error__: "Wrong"
+    }
+  };
+
+  expect(resolveMessage(messages, "foo")).toEqual("Wrong");
+});
+
+test("RepeatingFormAccessor error", () => {
+  // RepeatingFormAccessor "foo"
+  // path: foo
+  const messages = {
+    __error__foo: "Wrong"
+  };
+
+  expect(resolveMessage(messages, "foo")).toEqual("Wrong");
+});
 
 // RepeatingFormIndexed accessor 2 of RepeatingFormAccessor "foo"
 // path: foo/1
@@ -40,32 +86,4 @@ const repeatingFormError = {
       __error__: "Wrong"
     }
   ]
-};
-
-// FieldAccessor "bar"
-// on RepeatingFormIndexed accessor 2 of RepeatingFormAccessor "foo"
-// path: foo/1/bar
-const repeatingFormFieldError = {
-  foo: [
-    {},
-    {
-      bar: "Wrong"
-    }
-  ]
-};
-
-// SubFormAccessor "foo"
-// path: foo
-const subFormError = {
-  foo: {
-    __error__: "Wrong"
-  }
-};
-
-// FieldAccessor "bar" on SubFormAccessor "foo"
-// path: foo/bar
-const subFormFieldError = {
-  foo: {
-    bar: "Wrong"
-  }
 };
