3477c2dc0b268485d302d0a5d32397bbb3774f4a
Martijn Faassen <faassen@startifact.com>
2019-01-24 14:05:08 +0100

    Make it build properly.

--- src/references.ts
+++ src/references.ts
@@ -3,7 +3,9 @@ import { FieldAccessor } from "./field-accessor";
 import { Source } from "./source";
 import { DependentQuery } from "./form";
 
-export class References<SQ, DQ> {
+export type Query = {};
+
+export class References<SQ extends Query, DQ extends Query> {
   constructor(
     public accessor: FieldAccessor<any, any>,
     public source: Source<SQ & DQ>,
@@ -22,25 +24,24 @@ export class References<SQ, DQ> {
     }
   }
 
-  async load(searchQuery?: SQ): Promise<any[]> {
+  getFullQuery(searchQuery?: SQ): SQ & DQ {
     if (searchQuery == null) {
       searchQuery = {} as SQ;
     }
-    const fullQuery: SQ & DQ = {
-      ...searchQuery,
-      ...this.dependentQuery(this.accessor)
-    };
-    return this.source.load(fullQuery);
+    // it may be we don't need to do the 'as object' story anymore
+    // in TS 3.2
+    // https://github.com/Microsoft/TypeScript/wiki/What's-new-in-TypeScript#generic-spread-expressions-in-object-literals
+    return {
+      ...(searchQuery as object),
+      ...(this.dependentQuery(this.accessor) as object)
+    } as SQ & DQ;
+  }
+
+  async load(searchQuery?: SQ): Promise<any[]> {
+    return this.source.load(this.getFullQuery(searchQuery));
   }
 
   references(searchQuery?: SQ): any[] | undefined {
-    if (searchQuery == null) {
-      searchQuery = {} as SQ;
-    }
-    const fullQuery: SQ & DQ = {
-      ...searchQuery,
-      ...this.dependentQuery(this.accessor)
-    };
-    return this.source.references(fullQuery);
+    return this.source.references(this.getFullQuery(searchQuery));
   }
 }
