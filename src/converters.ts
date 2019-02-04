17d2fb160976e0cf928cf08136fa30c202df004c
Randy Wanga <r.wanga@isprojects.nl>
2019-02-04 13:41:22 +0100

    Reinsert trimDecimals into mstform.

--- src/converters.ts
+++ src/converters.ts
@@ -14,7 +14,8 @@ import {
   convertSeparators,
   DecimalOptions,
   getRegex,
-  renderSeparators
+  renderSeparators,
+  trimDecimals
 } from "./decimal";
 
 const NUMBER_REGEX = new RegExp("^-?(0|[1-9]\\d*)(\\.\\d*)?$");
@@ -125,7 +126,10 @@ function decimal(
       return raw;
     },
     render(value, converterOptions) {
-      return renderSeparators(value, converterOptions);
+      return renderSeparators(
+        trimDecimals(value, converterOptions, decimalOptions),
+        converterOptions
+      );
     }
   });
 }
