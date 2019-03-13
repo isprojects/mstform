3e8638125713631e0b45c86e9ebcacaae2651b30
GerjonM <g.mensinga@isprojects.nl>
2019-03-13 14:28:21 +0100

    setValueAndUpdateRaw

--- test/form.test.ts
+++ test/form.test.ts
@@ -2732,3 +2732,34 @@ test("form with thousandSeparator and decimalSeparator same value invalid", asyn
     });
   }).toThrow();
 });
+
+test("setValueAndUpdateRaw", async () => {
+  const M = types.model("M", {
+    foo: types.string
+  });
+
+  const o = M.create({ foo: "" });
+
+  const form = new Form(M, {
+    foo: new Field(converters.decimal())
+  });
+
+  const state = form.state(o, {
+    converterOptions: {
+      thousandSeparator: ",",
+      decimalSeparator: "."
+    }
+  });
+
+  // Setting the raw directly would update the value without relying on other event handlers
+  const field = state.field("foo");
+
+  await field.setRaw("1,000");
+  expect(field.raw).toEqual("1,000");
+  expect(field.value).not.toEqual("1,000");
+
+  // Instead, we set the value and update the raw based on the value
+  await field.setValueAndUpdateRaw("1,000");
+  expect(field.raw).toEqual("1,000");
+  expect(field.value).toEqual("1,000");
+});
