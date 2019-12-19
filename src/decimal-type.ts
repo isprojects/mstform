9bd0808097861472c365a696732b7f2ea9fb9875
Martijn Faassen <faassen@startifact.com>
2019-12-19 10:19:09 +0100

    Introduce new mst decimal type.

---
+++ src/decimal-type.ts
@@ -0,0 +1,23 @@
+import { Decimal } from "decimal.js-light";
+import { types } from "mobx-state-tree";
+
+export const decimal = types.custom<string, Decimal>({
+  name: "decimal",
+  fromSnapshot(value) {
+    return new Decimal(value);
+  },
+  toSnapshot(value) {
+    return value.toString();
+  },
+  isTargetType(value) {
+    return value instanceof Decimal;
+  },
+  getValidationMessage(snapshot) {
+    try {
+      const dummy = new Decimal(snapshot);
+    } catch (e) {
+      return "Not a valid decimal";
+    }
+    return "";
+  }
+});
