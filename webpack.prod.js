6ce651a0cf9715bacad297e149427e2f7e66d827
Mark den Toom <m.dentoom@isprojects.nl>
2021-09-16 16:41:04 +0200

    NEXTPL-156 enable terser minify for production builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,47 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
 const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
+  optimization: {
+    minimize: true,  // Only enable in production environment
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            // We want terser to parse ecma 8 code. However, we don't want it
+            // to apply any minification steps that turns valid ecma 5 code
+            // into invalid ecma 5 code. This is why the 'compress' and 'output'
+            // sections only apply transformations that are ecma 5 safe
+            // https://github.com/facebook/create-react-app/pull/4234
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: true,
+            comparisons: true,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          // Added for profiling in devtools
+          keep_classnames: true,  // Only enable in production environment
+          keep_fnames: true,  // Only enable in production environment
+          output: {
+            ecma: 5,
+            comments: false,
+            // Turned on because emoji and regex is not minified properly using default
+            // https://github.com/facebook/create-react-app/issues/2488
+            ascii_only: true,
+          },
+        },
+      }),
+    ],
+  },
   externals: {
     mobx: "mobx",
     "mobx-state-tree": "mobx-state-tree",
